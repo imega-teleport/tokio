0652700a79cc7e2593645cc5c7f8ba1220eabb1f
Dmitry <irvis@imega.ru>
2016-06-17 21:32:50 +0300

    Add check status rsync

--- src/daemon.sh
+++ src/daemon.sh
@@ -8,7 +8,7 @@ do
     UUID=`echo $(basename "$DEST")`
     case "$SERVICE" in
         "zip")
-            rsync --inplace -av ${DEST%?} rsync://extractor:873/data
+            rsync --inplace -av ${DEST%?} rsync://extractor:873/data && \
             rm -rf $DEST$FILE
         ;;
         "unzip") echo "rsync -avP $DEST$FILE rsync://10.0.0.2:873/data"
