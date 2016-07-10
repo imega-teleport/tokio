3c3568508b5f649eaebd36a24fe3a4371f1e6cab
Dmitry <irvis@imega.ru>
2016-07-10 14:12:43 +0300

    off debug

--- src/daemon.sh
+++ src/daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/rsync --no-detach -v --daemon --config /etc/rsyncd.conf &
+/usr/bin/rsync --no-detach --daemon --config /etc/rsyncd.conf &
 
 inotifywait -mr -e close_write --fromfile /app/wait-list.txt | while read DEST EVENT FILE
 do
@@ -8,11 +8,10 @@ do
     UUID=`echo $(basename "$DEST")`
     case "$SERVICE" in
         "zip")
-            rsync --inplace -av --debug=ALL $DEST$FILE rsync://extractor:873/data/$UUID/ && \
-            rm -rf $DEST$FILE
+            rsync --inplace -av $DEST$FILE rsync://extractor:873/data/$UUID/
         ;;
         "parse")
-            rsync --inplace -av --debug=ALL $DEST$FILE rsync://parser:873/data/$UUID/
+            rsync --inplace -av $DEST$FILE rsync://parser:873/data/$UUID/
         ;;
     esac
 done
